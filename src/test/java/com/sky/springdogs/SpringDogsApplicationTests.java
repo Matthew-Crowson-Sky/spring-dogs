385e171f10a359104e51afc6cbddb039e75be93d
Matthew-Crowson-Sky <matthew.crowson@sky.uk>
2023-07-31 10:04:50 +0100

    initial commit

---
+++ src/test/java/com/sky/springdogs/SpringDogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.springdogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
