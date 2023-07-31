b066f8366cb616a103da591b65586e491fb4acc3
Matthew-Crowson-Sky <matthew.crowson@sky.uk>
2023-07-31 14:45:39 +0100

    updates PathParams to RequestParams

--- src/main/java/com/sky/springdogs/rest/DogController.java
+++ src/main/java/com/sky/springdogs/rest/DogController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
-import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -56,11 +55,10 @@ public class DogController {
     @PatchMapping("update/{id}")
     public Dog update(
             @PathVariable Integer id,
-            @PathParam("name") String name,
-            @PathParam("breed") String breed,
-            @PathParam("age") Integer age ){
+            @RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "breed", required = false) String breed,
+            @RequestParam(value = "age", required = false) Integer age ){
         // if (id < 0 || id >= this.dogs.size()) return null;
-
         Dog upDog = this.dogs.get(id);
 
         if (name != null) upDog.setName(name);
